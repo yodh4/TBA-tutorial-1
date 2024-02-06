81c5576127ac11c758e8d69b4777f9456a4487a5
yodh4 <cyrilus2004@gmail.com>
2024-02-06 22:10:08 +0700

    Menambahkan decorator pada productListPage

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -29,6 +29,7 @@ public class ProductController {
         return "redirect:list";
     }
 
+    @GetMapping("/list")
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
