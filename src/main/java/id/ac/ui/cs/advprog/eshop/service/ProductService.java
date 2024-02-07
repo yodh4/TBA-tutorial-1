25bbbf49bfc1a838b5cb66a35c18eb460d574af9
yodh4 <cyrilus2004@gmail.com>
2024-02-07 08:16:34 +0700

    Menambahkan function untuk edit product

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -5,5 +5,7 @@ import java.util.List;
 
 public interface ProductService {
     public Product create(Product product);
+    public Product update(String productId, Product product);
+    public Product findById(String id);
     public List<Product> findAll();
 }
