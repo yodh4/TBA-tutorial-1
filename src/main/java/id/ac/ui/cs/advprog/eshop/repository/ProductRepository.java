18c9084b9e13eccd77d755fe33f7cd285fb80542
yodh4 <cyrilus2004@gmail.com>
2024-02-07 08:14:42 +0700

    Menambahkan function untuk edit product

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -10,13 +10,43 @@ import java.util.List;
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
+    static long ID = 0;
 
     public Product create(Product product) {
+        product.setProductID(Long.toString(ID));
+        ID++;
         productData.add(product);
+        printAllProducts();
+        return product;
+    }
+
+    public Product update(String productId, Product updatedProduct) {
+        Product product = findById(productId);
+        product.setProductName(updatedProduct.getProductName());
+        product.setProductQuantity(updatedProduct.getProductQuantity());
         return product;
     }
 
     public Iterator<Product> findAll() {
         return productData.iterator();
+    }
+
+    public Product findById(String id) {
+        for (Product product : productData) {
+            if (product.getProductID().equals(id)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public void printAllProducts() {
+        System.out.println("List of Products:");
+        for (Product product : productData) {
+            System.out.println("Product ID: " + product.getProductID());
+            System.out.println("Product Name: " + product.getProductName());
+            System.out.println("Product Quantity: " + product.getProductQuantity());
+            System.out.println("------------------------");
+        }
     }
 }
